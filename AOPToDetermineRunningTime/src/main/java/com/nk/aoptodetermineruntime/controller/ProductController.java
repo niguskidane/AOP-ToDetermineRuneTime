60fad9e23b407eee36c54fc5eeb791cbeca3cddb
King <nigus.kidane@gmail.com>
2019-09-07 12:02:03 -0600

    change added

--- AOPToDetermineRunningTime/src/main/java/com/nk/aoptodetermineruntime/controller/ProductController.java
+++ AOPToDetermineRunningTime/src/main/java/com/nk/aoptodetermineruntime/controller/ProductController.java
@@ -15,48 +15,48 @@ import java.util.List;
 @RestController
 public class ProductController {
 
-    private Logger logger= LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private ProductService productService;
 
-        @PostMapping("/products")
-        @LogRunningTime
-        public void addProduct(@RequestBody Product product){
-            logger.warn("addProduct input data : "+Utilities.objectToString(product));
-            productService.saveProduct(product);
-        }
+    @PostMapping("/products")
+    @LogRunningTime
+    public void addProduct(@RequestBody Product product) {
+        logger.warn("addProduct input data : " + Utilities.objectToString(product));
+        productService.saveProduct(product);
+    }
 
-        @GetMapping("/products/{productId}")
-        @LogRunningTime
-        public Product getProduct(@PathVariable("productId") String productId){
-            return productService.getSingleProduct(productId);
-        }
+    @GetMapping("/products/{productId}")
+    @LogRunningTime
+    public Product getProduct(@PathVariable("productId") String productId) {
+        return productService.getSingleProduct(productId);
+    }
 
-        @GetMapping("/products")
-        @LogRunningTime
-        public List<Product> getAllProduct(){
-            List<Product> products=new ArrayList<>();
-            try {
-                products=productService.getAll();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return products;
+    @GetMapping("/products")
+    @LogRunningTime
+    public List<Product> getAllProduct() {
+        List<Product> products = new ArrayList<>();
+        try {
+            products = productService.getAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        return products;
+    }
 
-        @PutMapping("/products/{productId}")
-        @LogRunningTime
-        public void updateProduct(@PathVariable("productId") String productId,@RequestBody Product product){
-            logger.warn("updateProduct input data : "+Utilities.objectToString(product));
-            productService.updateProduct(productId, product);
-        }
+    @PutMapping("/products/{productId}")
+    @LogRunningTime
+    public void updateProduct(@PathVariable("productId") String productId, @RequestBody Product product) {
+        logger.warn("updateProduct input data : " + Utilities.objectToString(product));
+        productService.updateProduct(productId, product);
+    }
 
-        @DeleteMapping("products/{productId}")
-        @LogRunningTime
-        public void deleteProduct(@PathVariable("productId") String productId){
-            productService.deleteProduct(productId);
-        }
+    @DeleteMapping("products/{productId}")
+    @LogRunningTime
+    public void deleteProduct(@PathVariable("productId") String productId) {
+        productService.deleteProduct(productId);
+    }
 
 
 }
